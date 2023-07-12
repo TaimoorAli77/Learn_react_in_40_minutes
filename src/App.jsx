5be5379ff314c24b19b93015f9a2b4f067b101ad
Taimoor Ali <taimoorali083@gmail.com>
2023-07-12 10:12:48 +0500

    "Working completely -no breaking into components"

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import NewTodoForm from "./NewTodoForm";
-// import TodoItem from "./TodoItem";
-import TodoList from "./TodoList";
 import "./styles.css";
 import { useState } from "react";
 export default function App() {
+  const [newItem, setNewItem] = useState("");
   const [todos, setTodos] = useState([]);
 
- function addTodo(title){
-  setTodos(newTodo=>{
-    return [...newTodo , {
-      id:crypto.randomUUID(),
-      title,
-      completed:false
-    }]
-  })
- }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos((ntodos) => {
+      return [
+        ...ntodos,
+        {
+          id: crypto.randomUUID(),
+          title: newItem,
+          completed: false,
+        },
+      ];
+    });
+
+    setNewItem("");
+  };
 
 
   function toggleTodo(id , completed){
@@ -44,9 +48,43 @@ export default function App() {
 
   return (
     <>
-      <NewTodoForm      addTodo={addTodo} />
-
-      <TodoList  todos={todos}/>
+      <form className="new-item-form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="new-item">New Item</label>
+          <input
+            className="input"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
+          <button className="btn">Add</button>
+        </div>
+      </form>
+      <h1 className="header">Todo List</h1>
+      {todos.length===0 && "No Tasks"}
+      {todos.map((todo) => {
+        return (
+          <ul className="list" key={todo.id}>
+            <li>
+              <label htmlFor="list">
+                <input
+                  type="checkbox"
+                  className="box-round"
+                  checked={todo.completed}
+                  onChange={e=>toggleTodo(todo.id, e.target.checked)}
+                />
+               <span>
+                {todo.title}
+                </span> 
+              </label>
+              <button className="btn-danger" onClick={()=>deleteTodo(todo.id)}>
+                Delete
+              </button>
+            </li>
+          </ul>
+        );
+      })}
     </>
   );
 }
